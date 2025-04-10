4551f4f3c2ab00bd5fb92110b8b5696cfe8f0280
Gug Daniel Flaviu <gfsoft03@gmail.com>
2025-04-10 21:42:53 +0300

    created fisaservice

--- templates/navbar.php
+++ templates/navbar.php
@@ -12,7 +12,7 @@
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary " data-bs-theme="dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">DeForge ERP</a>
+            <a class="navbar-brand" href="home.php">DeForge ERP</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -20,7 +20,7 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item ">
-                        <a class="nav-link" aria-current="page" href="#">FISA SERVICE</a>
+                        <a class="nav-link" aria-current="page" href="fisaservice.php">FISA SERVICE</a>
                     </li>
 
                 </ul>
