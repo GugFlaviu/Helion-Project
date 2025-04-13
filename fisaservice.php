ea4eea2a55f05313e4657f72b8d048480d711857
Gug Daniel Flaviu <gfsoft03@gmail.com>
2025-04-13 21:24:58 +0300

    i almost finished the form page

--- fisaservice.php
+++ fisaservice.php
@@ -31,33 +31,57 @@ include "./config/sqlconnect.php";
     include "./config/sqlconnect.php";
     include "./templates/navbar.php";
 
-    $sql = "SELECT * FROM detalii_firma";
-    $result = $connect->query($sql);
+
     ?>
     <section class="container w-50 align-items-start">
 
-        <div class="container title-container">
-            <h1 class="text-title-container">Fise Helion Security</h1>
+        <div class="container title-container ">
+            <h1 class="text-title-container">FISA HELION SECURITY</h1>
             <p class="text-title-container">Proces verbal de predare-primire
             </p>
             <p class="text-title-container">Nr. Fisa/P.V: S - <strong><?php echo $invoice_number; ?></strong>
             </p>
             <h3>Service fara Contract</h3>
         </div>
+        <br>
         <div class="container d-flex justify-content-start gap-3">
-            <div class="w-33 h-25">
-                <h5 class="test">Detalii firma:</h5>
+            <div class="w-33 h-25  ">
+                <h3>Detalii firma:</h3>
                 <input type="text" class="form-control mb-2" placeholder="Cauta firma ...">
 
-                <div class="overflow-auto scroll-box">
+                <div class="overflow-auto  " style="height: 250px;">
 
                     <?php
+                    $sql = "SELECT * FROM detalii_firma";
+                    $result = $connect->query($sql);
                     while ($row = $result->fetch_assoc()) {
                         echo "<input class='form-check-input' type='checkbox'>
-                    <div class='form-check-label'>
+                    <div class='form-check-label '>
                         <strong>$row[nume_firma]</strong><br>
                         <p>CUI: $row[cui]</p>
-                    </div>";
+                    </div><hr>";
+                    }
+
+                    ?>
+
+                </div>
+            </div>
+            <div class="w-33 gap-2 pb-4 ">
+                <h3>Punct de lucru:</h3>
+                <input type="text" class="form-control mb-2" placeholder="Cauta punct de lucru ...">
+                <div class="overflow-auto  " style="height: 250px;">
+
+                    <?php
+                    $sql = "SELECT * FROM punct_lucru";
+                    $result = $connect->query($sql);
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<input class='form-check-input' type='checkbox'>
+                    <div class='form-check-label '>
+                        <strong>$row[nume]</strong><br>
+                        <p>ZONA: $row[zona]</p>
+                        <p>Oras: $row[oras]</p>
+                        <p>Adresa: $row[adresa]</p>
+                    </div><hr>";
                     }
 
                     ?>
@@ -65,19 +89,262 @@ include "./config/sqlconnect.php";
                 </div>
             </div>
             <div class="w-33 gap-2">
-                <h5 class="test">Detalii firma:</h5>
-                <input type="text" class="form-control mb-2" placeholder="Cauta firma ...">
+                <h3>Reprezentat:</h3>
+                <div class="" style="height: 250px;">
+
+                    <?php
+                    $sql = "SELECT * FROM reprezentant";
+                    $result = $connect->query($sql);
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<input class='form-check-input' type='checkbox'>
+                                <div class='form-check-label '>
+                                    <strong>$row[nume]</strong><br>
+                                    <p>Telefon: 0$row[telefon]</p>
+                                    <p>Email: $row[email]</p>
+                                    <p>Functie: $row[functie]</p>
+                                </div><hr>";
+                    }
+
+                    ?>
+                    <input class='form-check-input' type='checkbox'>
+                    <div class='form-check-label '>
+                        <strong>Alta Persoana</strong><br>
+
+                    </div>
+                    <hr>
+                </div>
+            </div>
+
+        </div>
+        <hr>
+        <br>
+        <div class="container d-flex justify-content-start gap-3">
+            <div class="w-33 h-25  ">
+                <h3>Tip Sistem</h3>
+
+
+                <div class="overflow-auto  " style="height: 250px;">
+
+                    <?php
+                    $sql = "SELECT * FROM tip_sistem";
+                    $result = $connect->query($sql);
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<input class='form-check-input' type='checkbox'>
+                    <div class='form-check-label '>
+                        <strong>$row[nume]</strong><br>
+                      
+                    </div><hr>";
+                    }
+
+                    ?>
+
+                </div>
+            </div>
+            <div class="container">
+                <div class="row align-items-start">
+                    <div class="col-md-6 mb-3">
+                        <h3>Punctul detine jurnal?</h3>
+
+                        <select class="form-select w-50" id="jurnal">
+                            <option>Alege</option>
+                            <option>Da</option>
+                            <option>Nu</option>
+                        </select>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="formFile" class="form-label">Ataseaza poza</label>
+                        <input class="form-control mb-2" type="file" id="formFile">
+                        <button class="btn btn-primary" id="clearFile">Sterge poza</button>
+                    </div>
+
+                </div>
+
+            </div>
+
+
+            <br>
+        </div>
+        <hr>
+        <br>
+        <div class="container d-flex justify-content-start gap-3">
+            <div class="w-33 h-25  ">
+                <h3>Defect semnalat
+                    :</h3>
+
+
+                <div class="overflow-auto  " style="height: 150px;">
+
+                    <textarea class="form-control" aria-label="With textarea"></textarea>
+
+                </div>
+            </div>
+            <div class="w-33 h-25  ">
+                <h3>Contestatie
+                    :</h3>
+
+
+                <div class="overflow-auto  " style="height: 150px;">
+
+                    <textarea class="form-control" aria-label="With textarea"></textarea>
+
+                </div>
+            </div>
+
+
+        </div>
+        <hr>
+        <br>
+        <div class="container d-flex justify-content-start gap-3">
+            <div class="w-33 h-25  ">
+                <h3>Info Service
+                    :</h3>
+                <div class="input-group ">
+                    <span class="input-group-text w-50">Preluat in service</span>
+                    <select class="form-select w-50" id="jurnal">
+                        <option>Alege</option>
+                        <option>Nu</option>
+                        <option>Da</option>
+
+                    </select>
+                </div>
+                <div class="input-group ">
+                    <span class="input-group-text w-50">Aparat reparat</span>
+                    <select class="form-select w-50" id="jurnal">
+                        <option>Alege</option>
+                        <option>Nu</option>
+                        <option>Da</option>
+
+                    </select>
+                </div>
+                <div class="input-group">
+                    <span class="input-group-text w-50" style="color:grey">Pret estimativ</span>
+                    <span class="input-group-text w-50" style="color:grey">Ex: 150 lei</span>
+                </div>
+            </div>
+            <div class="w-33 h-25  ">
+                <h3>OPERAȚII:</h3>
+                <div class="overflow-auto  " style="height: 150px;">
+                    <textarea class="form-control" aria-label="With textarea"></textarea>
+                </div>
+            </div>
+            <div class="w-33 h-25  ">
+                <h3>Consum:</h3>
+                <div class="overflow-auto  " style="height: 150px;">
+                    <textarea class="form-control" aria-label="With textarea"></textarea>
+                </div>
+            </div>
+
+        </div>
+        <hr>
+        <br>
+        <div class="container d-flex justify-content-start gap-3">
+            <div class="w-33 h-25  ">
+                <h3>Recomandari
+                </h3>
+                <div class="overflow-auto  " style="height: 150px;">
+                    <textarea class="form-control" aria-label="With textarea"></textarea>
+                </div>
+
+
+            </div>
+            <div class="w-33 gap-2 pb-4 ">
+                <h3>Inginer helion :</h3>
+                <input type="text" class="form-control mb-2" placeholder="Cauta punct de lucru ...">
+                <div class="overflow-auto  " style="height: 250px;">
+
+                    <?php
+                    $sql = "SELECT * FROM inginer";
+                    $result = $connect->query($sql);
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<input class='form-check-input' type='checkbox'>
+                    <div class='form-check-label '>
+                        <strong>$row[nume]</strong><br>
+                        <p>Functie: $row[functie]</p>
+                       
+                    </div><hr>";
+                    }
+
+                    ?>
+
+                </div>
             </div>
             <div class="w-33 gap-2">
-                <h5 class="test">Detalii firma:</h5>
-                <input type="text" class="form-control mb-2" placeholder="Cauta firma ...">
+                <h3>Reprezentat:</h3>
+                <div class="" style="height: 250px;">
+
+
+                    <input class='form-check-input' type='checkbox'>
+                    <div class='form-check-label '>
+                        <strong>Alta Persoana</strong><br>
+
+                    </div>
+                    <hr>
+                </div>
             </div>
+
+        </div>
+        <hr>
+        <br>
+        <div class="container d-flex justify-content-start gap-3">
+            <div class="w-50 h-25 ">
+                <h3>Data Predarii:</h3>
+                <div class="overflow-auto">
+                    <input type="date" class="form-control">
+                    <div class="input-group flex-nowrap mt-3 m-0 ">
+                        <p class="input-group-text w-100 m-0  " style="color:gray;">
+                            Timp transport (spre și de la obiectiv)
+                        </p>
+                    </div>
+
+                    <div class="input-group mb-3 m-0 ">
+                        <input type="text" class="form-control m-0  " placeholder="Ex:20">
+                        <span class="input-group-text w-50 m-0  ">min</span>
+                    </div>
+                </div>
+                <div class="overflow-auto">
+
+                    <div class="input-group flex-nowrap mt-3 m-0 ">
+                        <p class="input-group-text w-100 m-0  " style="color:gray;">
+                            Kilometri parcursi spre si de la obiectiv
+                        </p>
+                    </div>
+
+                    <div class="input-group mb-3 m-0 ">
+                        <input type="text" class="form-control m-0  " placeholder="Ex:110">
+                        <span class="input-group-text w-50 m-0  ">km</span>
+                    </div>
+                </div>
+                <div class="overflow-auto">
+
+                    <div class="input-group flex-nowrap mt-3 m-0 ">
+                        <p class="input-group-text w-100 m-0  " style="color:gray;">
+                            Timp manopera
+                        </p>
+                    </div>
+
+                    <div class="input-group mb-3 m-0 ">
+                        <input type="text" class="form-control m-0  " placeholder="Ex:10">
+                        <span class="input-group-text w-50 m-0  ">min</span>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+
+
         </div>
 
     </section>
 
-
-
+    <script>
+        document.getElementById("clearFile").addEventListener("click", function (e) {
+            e.preventDefault(); // Prevent form submission if inside a form
+            document.getElementById("formFile").value = "";
+        });
+    </script>
+    <footer style="height:100px;"></footer>
 </body>
 
 </html>
